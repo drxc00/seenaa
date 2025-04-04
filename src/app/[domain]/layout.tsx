43606308a2f0db6d0ab9ffeec004f4e949d12ef6
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-04-04 20:24:32 +0800

    feat: enhance layout and blog page UI with improved styling and new icons

--- src/app/[domain]/layout.tsx
+++ src/app/[domain]/layout.tsx
@@ -2,9 +2,9 @@ import { PiPaperPlaneThin } from "react-icons/pi";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
+    <main className="bg-muted flex flex-col min-h-screen justify-between">
       {children}
-      <div className="flex flex-col items-center justify-center py-4">
+      <div className="flex flex-col justify-center items-center py-4">
         <div className="flex flex-row items-center justify-center gap-2 text-sm font-semibold text-muted-foreground">
           <PiPaperPlaneThin className="h-6 w-6" />
           <span>seenaa</span>
--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getBlogData, isSubDomainValid } from "@/data/domain-dal";
-import { Calendar } from "lucide-react";
+import { ArrowRight, Calendar, Send } from "lucide-react";
 import Link from "next/link";
 
 type BlogData = Awaited<ReturnType<typeof getBlogData>>;
@@ -20,29 +20,41 @@ export default async function BlogPage({ params }: BlogPageProps) {
   const blogData: BlogData = await getBlogData(domain);
 
   return (
-    <main className="min-h-screen w-full max-w-5xl mx-auto py-16 px-4 sm:px-6">
-      <div className="flex flex-col items-center mb-12 space-y-4">
-        <Avatar className="w-20 h-20 border-2 border-primary/10 shadow-sm">
-          <AvatarImage src={blogData?.user.image || ""} alt={domain} />
-          <AvatarFallback className="text-lg font-medium bg-primary/5">
-            {blogData?.user.name?.[0] || ""}
-          </AvatarFallback>
-        </Avatar>
-        <h1 className="font-bold text-3xl tracking-tight">
-          {blogData?.user.name}
-        </h1>
-        {/* {blogData?.user.bio && (
-          <p className="text-muted-foreground text-center max-w-md">
-            {blogData.user.bio}
-          </p>
-        )} */}
-      </div>
+    <main className="w-full max-w-5xl mx-auto py-10 px-4 sm:px-6">
+      <Card className="w-full p-0 shadow-none border-dashed">
+        <CardContent className="p-4">
+          <div className="flex flex-col items-center space-y-4">
+            <Avatar className="w-14 h-14 border-1 border-primary/10">
+              <AvatarImage src={blogData?.user.image || ""} alt={domain} />
+              <AvatarFallback className="text-lg font-medium bg-primary/5">
+                {blogData?.user.name?.[0] || ""}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col items-center space-y-1">
+              <h1 className="font-bold text-xl tracking-tight lowercase">
+                {blogData?.user.name}
+              </h1>
+              <p className="text-muted-foreground text-md font-medium">
+                @{blogData?.user.username}
+              </p>
+            </div>
+            {blogData?.user.bio && (
+              <p className="text-muted-foreground text-center max-w-md">
+                {blogData.user.bio}
+              </p>
+            )}
+          </div>
+        </CardContent>
+      </Card>
 
-      <section className="mb-16">
-        <div className="flex items-center justify-between mb-8">
-          <h2 className="text-2xl font-semibold tracking-tight">
-            Recent Posts
-          </h2>
+      <section className="mb-8 mt-16">
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center gap-2">
+            <Send className="w-6 h-6" />
+            <h2 className="text-2xl font-semibold tracking-tight">
+              recent posts
+            </h2>
+          </div>
           {blogData?.posts?.length && blogData?.posts?.length > 6 && (
             <Button variant="ghost" size="sm">
               View all
@@ -54,18 +66,12 @@ export default async function BlogPage({ params }: BlogPageProps) {
           {blogData?.posts.map((post) => (
             <Card
               key={post?.id}
-              className="overflow-hidden transition-all duration-200 hover:shadow-md p-0"
+              className="overflow-hidden shadow-none transition-all duration-200 hover:shadow-sm p-0"
             >
               <CardContent className="p-4 flex flex-col gap-3">
-                <h3 className="text-xl font-bold line-clamp-2 leading-tight">
+                <h3 className="text-lg font-semibold line-clamp-2 leading-tight">
                   {post?.title}
                 </h3>
-
-                {/* {post?.excerpt && (
-                  <p className="text-muted-foreground text-sm line-clamp-3">
-                    {post.excerpt}
-                  </p>
-                )} */}
                 <div className="flex items-center justify-between">
                   <div className="flex items-center gap-2 text-sm text-muted-foreground">
                     <Calendar className="h-3.5 w-3.5" />
@@ -80,7 +86,8 @@ export default async function BlogPage({ params }: BlogPageProps) {
 
                   <Link href={`/${post?.slug}`}>
                     <Button variant="ghost" size="sm" className="font-medium">
-                      Read post
+                      <span>Read post</span>
+                      <ArrowRight className="h-4 w-4" />
                     </Button>
                   </Link>
                 </div>
