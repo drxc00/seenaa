2236b0a5be764f22950dfa172f34209cbf0eac52
Neil Patrick Villanueva <neilpatz16@gmail.com>
2025-04-07 17:36:21 +0800

    fix: increase gap between blog cards for improved layout

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -18,7 +18,7 @@ export default async function PublicBlogs() {
           <div className="flex flex-col items-center justify-center mb-8">
             <h1 className="text-2xl font-semibold md:text-3xl">blogs</h1>
           </div>
-          <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
             {allBlogs.map((blog) => (
               <BlogCard blog={blog} key={blog.username} />
             ))}
